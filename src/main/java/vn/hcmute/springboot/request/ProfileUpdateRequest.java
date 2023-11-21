8c6074780ce53d84ad628d58de6bc78b33207dca
vopn010202 <namvo.010202@gmail.com>
2023-11-21 07:58:34 +0700

    fix profile response
    Signed-off-by: vopn010202 <namvo.010202@gmail.com>

--- src/main/java/vn/hcmute/springboot/request/ProfileUpdateRequest.java
+++ src/main/java/vn/hcmute/springboot/request/ProfileUpdateRequest.java
@@ -7,16 +7,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.util.List;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.lang.Nullable;
-import org.springframework.web.multipart.MultipartFile;
-import vn.hcmute.springboot.model.CandidateExperience;
 import vn.hcmute.springboot.model.Gender;
-import vn.hcmute.springboot.model.Skill;
 
 @Getter
 @Setter
@@ -31,7 +25,7 @@ public class ProfileUpdateRequest {
   private String position;
   @NotNull
   private String phoneNumber;
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
   private LocalDate birthdate;
   private String linkWebsiteProfile;
   private String city;
--- src/main/java/vn/hcmute/springboot/response/UserProfileResponse.java
+++ src/main/java/vn/hcmute/springboot/response/UserProfileResponse.java
@@ -3,14 +3,11 @@ package vn.hcmute.springboot.response;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
-import vn.hcmute.springboot.model.CandidateEducation;
-import vn.hcmute.springboot.model.CandidateExperience;
 import vn.hcmute.springboot.model.Gender;
 import vn.hcmute.springboot.model.UserStatus;
 
@@ -27,6 +24,7 @@ public class UserProfileResponse {
   private String address;
   private String position;
   private String phoneNumber;
+  @JsonFormat(pattern = "dd/MM/yyyy")
   private LocalDate birthdate;
   private String linkWebsiteProfile;
   private List<String> skills;
